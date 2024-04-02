f79129a53f86f5153fc5e27c8452b94ec48d3b02
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-04-02 09:23:53 -0300

    Add files via upload

---
+++ Assets/scriptMataParticula.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptMataParticula : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Invoke ("MataParticula", 2.5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void MataParticula(){
+
+		Destroy (gameObject);
+	}
+}
